27fa5fcb01db7ebd7cc04eb0b7f8d0c022eca031
shibadogcap <yuuki1315s@gmail.com>
2025-03-04 08:15:48 +0900

    update script

--- main.py
+++ main.py
@@ -29,7 +29,9 @@ sys_instruct = """
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("gemini-2.0-flash")
 chat = model.start_chat(history=[
-    sys_instruct,
+    {'role': 'user',
+      'parts': [sys_instruct]
+    }
 ])
 
 ### discord initial

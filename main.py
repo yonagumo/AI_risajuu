522421f30737fbbcb2a7ffadb46f3f784fb2248b
shibadogcap <yuuki1315s@gmail.com>
2025-03-30 20:11:53 +0900

    fix typo

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ risajuu_image = [
 
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
 search_tool = {'google_search': {}}
-model = genai.GenerativeModel("gemini-2.0-flash-thinking-exp")
 chat = client.start_chat(
     model = "gemini-2.0-flash-thinking-exp",
     system_instruction = sys_instruct,

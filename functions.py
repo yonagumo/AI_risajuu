2a20864d2b9babf6c91086e31fdc6ff54bff6d31
yonagumo <yonagumo.dev@gmail.com>
2025-06-11 19:57:14 +0900

    実験用weather関数をアップデートじゅう

--- functions.py
+++ functions.py
@@ -21,22 +21,31 @@ get_weather_forecast_declaration = {
 
 
 def get_weather_forecast(location: str) -> dict[str, str | int]:
+    if random.random() < 0.1:
+        if location in ["調布", "調布市"]:
+            raise Exception("データ取得に失敗")
+        else:
+            raise Exception("未対応の地域")
+
     temperature = random.randint(-10, 40)
+
     match random.randint(1, 3):
         case 1:
             weather = "晴れ"
         case 2:
-            weather = "曇り"
-            raise Exception
+            if random.random() < 0.1:
+                weather = "濃霧"
+            else:
+                weather = "曇り"
         case 3:
             if temperature <= -3:
                 weather = "雪"
             elif temperature <= 3:
                 weather = "みぞれ"
             else:
-                if random.random() < 0.9:
-                    weather = "雨"
-                else:
+                if random.random() < 0.1:
                     weather = "雷雨"
+                else:
+                    weather = "雨"
 
     return {"weather": weather, "temperature": temperature}
